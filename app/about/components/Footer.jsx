c89ee16b077bf0a16ded13c2141e47bdea833fe7
Nihaal-Ahmed <npawaskar5@gmail.com>
2025-10-21 19:51:49 +0530

    add components/Footer

---
+++ app/about/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ marginTop: 40, borderTop: '1px solid #eee', paddingTop: 12 }}>
+      <small>© {new Date().getFullYear()} My Next App</small>
+    </footer>
+  )
+}
