268e795d0d05c42144cd47819860c86420686386
Nihaal-Ahmed <npawaskar5@gmail.com>
2025-10-21 19:50:05 +0530

    add app/about page

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import Nav from '../../components/Nav'
+import Footer from '../../components/Footer'
+
+export default function About() {
+  return (
+    <>
+      <Nav />
+      <main>
+        <h1>About</h1>
+        <p>This is a simple About page inside the app router.</p>
+      </main>
+      <Footer />
+    </>
+  )
+}
